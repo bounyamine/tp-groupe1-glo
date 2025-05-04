dbd6ac67f66253d414cf6f631a6d26e6ee6bf360
bounyamine <ousmanoubounyamine@gmail.com>
2025-05-04 11:37:22 +0100

    Ajout de la classe Point - par BOUNYAMINE OUSMANOU

---
+++ src/globals/Point.java
@@ -0,0 +1,17 @@
+public class Point {
+    private double abs;
+    private double ord;
+    
+    public Point(double abs, double ord) {
+        this.abs = abs;
+        this.ord = ord;
+    }
+    
+    public double getAbs() {
+        return abs;
+    }
+    
+    public double getOrd() {
+        return ord;
+    }
+}
